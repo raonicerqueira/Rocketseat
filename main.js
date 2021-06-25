09169faa4cbd828caac826ec6aaaec075c352405
raonicerqueira <raoni_henrique@live.com>
2021-06-24 21:14:26 -0300

    Aula 03

--- main.js
+++ main.js
@@ -31,3 +31,32 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+/*Testimonials carousel slider swiper*/
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/*ScrollReveal: Mostrar elementros quando der scroll na página*/
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+#home .image, #home .text,
+#about .image, #about .text,
+#services .header, #services .card,
+#testimonials header, #testimonials .testimonials
+#contact .text, #contact .links
+`,
+  { interval: 100 }
+)
